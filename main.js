0cbfe22ba495e822ab435d17ea2fc31d23cefe86
Patinya-cha <patinya.cha04@gmail.com>
2025-07-21 11:55:01 +0700

    Morning Checkpoint

--- main.js
+++ main.js
@@ -20,18 +20,61 @@ class Field {
 		this.positionRow = 0;
 		this.positionCol = 0;
 		this.field[this.positionRow][this.positionCol] = pathCharacter;
+		this.gameOver = false;
 	}
 
 	// Print field //
 	print() {
 		clear();
-
-		// Replace with your own code //
-		console.log(this.field); // Please REMOVE this line before you start your code!
+		for (let row = 0; row < this.field.length; row++){
+			console.log(this.field[row]);
+		}
+	}
+	// Your Code //
+	updateMap(){
+		this.field[this.positionRow][this.positionCol] = pathCharacter;
+	}
+	move(direction){
+		if (direction === "w") {
+			this.moveUp();
+		}else if (direction === 's'){
+			this.moveDown();
+		}else if (direction === 'a'){
+			this.moveLeft();
+		}else if(direction === 'd'){
+			this.moveRight();
+		}
+		this.updateMap();
+		this.checkStatusGame();
 	}
 
-	// Your Code //
+	moveUp() {
+		this.positionRow --
+	}
+	moveDown() {
+		this.positionRow ++
+	}
+	moveLeft() {
+		this.positionCol --
+	}
+	moveRight() {
+		this.positionCol ++
+	}
+	checkStatusGame(){
+		const nowHere = this.field[this.positionRow]?.[this.positionCol];
+		if (nowHere === hole) {
+			console.log('Game Over! (you fell in the hol)d')
+			this.gameOver = true
+		}else if ( nowHere === hat){
+			console.log('You won!')
+			this.gameOver = false
+		}else if (nowHere === undefined)
+			console.log("You out of path) ")
+			this.gameOver = true
+		}
+
 }
+
 
 // Game Mode ON
 // Remark: Code example below should be deleted and use your own code.
